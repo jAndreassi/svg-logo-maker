91139532a4bda3091c69a21bcc23ce9cfa5cb055
Jared A <jroberts1931@gmail.com>
2023-03-04 23:04:13 -0500

    populate one shape with color, text and text color

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-inquirer.createPromptModule({
-  type: "input",
-  message: "What 3 characters do you want on your logo?",
-  name: 'text'
-},
-{
-type: 'list',
-message: 'Please choose a shape?',
-options: ['square', 'circle', 'triangle'],
-name: 'shape',
-
-},
-{
-  type: 'input',
-  message: 'What color do you want the shape to be?',
-  name: 'shapecolor',
-},
-{
-  type: 'input',
-  message: 'What color do you want your text to be?'
-  name: 'textcolor',
-}
-);
-
-fs.writeFile(
-  "./examples/logo.svg",
-  `<?xml version="1.0" standalone="no"?>
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What 3 characters do you want on your logo?",
+      name: "text",
+    },
+    // {
+    //   type: "list",
+    //   message: "Please choose a shape?",
+    //   options: ["square", "circle", "triangle"],
+    //   name: "shape",
+    // },
+    {
+      type: "input",
+      message: "What color do you want the shape to be?",
+      name: "shapecolor",
+    },
+    {
+      type: "input",
+      message: "What color do you want your text to be?",
+      name: "textcolor",
+    },
+  ])
+  .then((answers) =>
+    fs.writeFile(
+      "./examples/logo.svg",
+      `<?xml version="1.0" standalone="no"?>
 <svg xmlns="http://www.w3.org/2000/svg">
 <g>
-<circle cx="55" cy="75" r="50" fill ="bisque"></circle>
-<text x="15" y="75" r="50" font-size="20px" fill="blueviolet">Hello</text>
+<circle cx="55" cy="75" r="50" fill ="${answers.shapecolor}"></circle>
+<text x="15" y="75" r="50" font-size="20px" fill="${answers.textcolor}">${answers.text}</text>
 </g>
 
 </svg>
 </xml>`,
-  (err) => (err ? console.log(err) : console.log("Here is your logo"))
-);
+      (err) => (err ? console.log(err) : console.log("Here is your logo"))
+    )
+  );
